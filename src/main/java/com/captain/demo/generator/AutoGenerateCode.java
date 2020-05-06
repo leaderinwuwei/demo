02be79de614baec353eb9df168e56a79ca6edf30
无为大队长 <35330556+leaderinwuwei@users.noreply.github.com>
2020-05-06 10:41:39 +0800

    fix:code optimization

--- src/main/java/com/captain/demo/generator/AutoGenerateCode.java
+++ src/main/java/com/captain/demo/generator/AutoGenerateCode.java
@@ -1,182 +1,1 @@
-/**
- * pxys
- * Copyright (C) 2017-2019 All Rights Reserved.
- */
-package com.captain.demo.generator;
-
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
-import com.baomidou.mybatisplus.generator.config.rules.DateType;
-import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
-import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
-import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import org.apache.commons.lang.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Component;
-
-/**
- * @ClassName: AutoGenerateCode
- * @Description:
- * @Author: Monta
- * @Date: 2019/11/11 下午4:08
- **/
-@Component
-public class AutoGenerateCode {
-
-    private static Logger logger = LogManager.getLogger(AutoGenerateCode.class);
-
-    public static void main(String[] args) {
-        /**
-         * 根路径，我这里先直接放到此项目中。可以根据自己需要修改制定
-         */
-        String basePath = System.getProperty("user.dir");
-        String parentPackageName = "com.captain.demo";
-        String moduleName = "mvc";
-        String[] tableNames = new String[]{"d_user"};
-        String[] tablePres = new String[]{"d_"};
-        String jdbcHost = "127.0.0.1";
-        String jdbcPort = "3306";
-        String jdbcDatabase = "demo";
-        String jdbcUrl = String.format("jdbc:mysql://%s:%s/%s?characterEncoding=utf8&useSSL=false&autoReconnect=true&serverTimezone=Asia/Shanghai",
-                jdbcHost, jdbcPort, jdbcDatabase);
-        System.out.println(jdbcUrl);
-        AutoDataSourceConfig autoDataSourceConfig = AutoDataSourceConfig.builder()
-                .jdbcDriverClassName("com.mysql.jdbc.Driver")
-                .jdbcPassword("root@localHost")
-                .jdbcUserName("139566")
-                .jdbcUrl(jdbcUrl)
-                .build();
-        String outputDir = String.format("%s/src/main/java", basePath);
-        new AutoGenerateCode().generateCode(outputDir, parentPackageName,
-                moduleName, autoDataSourceConfig, tablePres, tableNames);
-    }
-
-
-    /**
-     * 生成代码
-     *
-     * @param outputDir         输出目录
-     * @param parentPackageName 父级包
-     * @param moduleName        模块名称
-     * @param dataSourceConfig  数据源配置
-     * @param tablePres         表前缀
-     * @param tableNames        待生成表名
-     */
-    private void generateCode(String outputDir, String parentPackageName,
-                              String moduleName, AutoDataSourceConfig dataSourceConfig, String[] tablePres, String[] tableNames) {
-        try {
-            AutoGenerator autoGenerator = new AutoGenerator();
-            autoGenerator.setGlobalConfig(initGlobConfig(outputDir))
-                    .setDataSource(initDataSource(dataSourceConfig))
-                    .setStrategy(initStrategyConfig(tablePres, tableNames))
-                    .setPackageInfo(initPackage(parentPackageName, moduleName))
-                    .execute();
-            logger.error(">>>>>>> Generate Code Completed... >>>>>>>");
-        } catch (Exception e) {
-            logger.error("Generate Code failed:", e);
-            throw new RuntimeException(String.format("Generate Code failed"));
-        }
-    }
-
-    /**
-     * 全局设置配置
-     *
-     * @param outputDir 生成文件输出根目录
-     * @return GlobalConfig
-     */
-    private GlobalConfig initGlobConfig(String outputDir) {
-        return new GlobalConfig()
-                .setOutputDir(outputDir)
-                .setFileOverride(false)
-                .setOpen(false)
-                .setEnableCache(false)
-                .setKotlin(false)
-                .setSwagger2(true)
-                .setDateType(DateType.ONLY_DATE)
-                .setActiveRecord(false)
-                .setBaseResultMap(true)
-                .setBaseColumnList(true)
-                .setMapperName("%sMapper")
-                .setXmlName("%sMapper")
-                .setServiceName("I%sService")
-                .setServiceImplName("%sServiceImpl")
-                .setControllerName("%sController")
-                .setAuthor("captain")
-                .setIdType(IdType.ID_WORKER);
-    }
-
-    /**
-     * 设置数据源信息
-     *
-     * @param dataSourceConfig 数据源配置
-     * @return DataSourceConfig
-     */
-    private DataSourceConfig initDataSource(AutoDataSourceConfig dataSourceConfig) {
-        return new DataSourceConfig()
-                .setDbType(DbType.MYSQL)
-                .setTypeConvert(new MySqlTypeConvert() {
-                    /**
-                     * 自定义部分数据库类型对应转换的Java类型，根据个人需要设定
-                     */
-                    @Override
-                    public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
-                        if (fieldType.toLowerCase().contains("tinyint")) {
-                            return DbColumnType.BOOLEAN;
-                        }
-                        return super.processTypeConvert(globalConfig, fieldType);
-                    }
-                })
-                .setDriverName(dataSourceConfig.getJdbcDriverClassName())
-                .setUrl(dataSourceConfig.getJdbcUrl())
-                .setUsername(dataSourceConfig.getJdbcUserName())
-                .setPassword(dataSourceConfig.getJdbcPassword());
-    }
-
-    /**
-     * 设置生成策略
-     *
-     * @param tablePre   表前缀
-     * @param tableNames 待生成表列表
-     * @return StrategyConfig
-     */
-    private StrategyConfig initStrategyConfig(String[] tablePre, String[] tableNames) {
-        return new StrategyConfig()
-                .setNaming(NamingStrategy.underline_to_camel)
-                .setColumnNaming(NamingStrategy.underline_to_camel)
-                .setTablePrefix(tablePre)
-                .setControllerMappingHyphenStyle(true)
-                .setEntityTableFieldAnnotationEnable(true)
-                .setEntityLombokModel(true)
-                .setInclude(tableNames)
-                .setRestControllerStyle(false);
-    }
-
-    /**
-     * 设置包配置信息
-     *
-     * @param parentPackageName 父级包
-     * @param moduleName        模块名称
-     * @return PackageConfig
-     */
-    private PackageConfig initPackage(String parentPackageName, String moduleName) {
-        logger.info(moduleName);
-        PackageConfig packageConfig = new PackageConfig()
-                .setParent(parentPackageName)
-                .setEntity("entity")
-                .setService("service")
-                .setServiceImpl("service.impl")
-                .setMapper("mapper")
-                .setXml("mapper.xml")
-                .setController("controller");
-        if (StringUtils.isNotEmpty(moduleName)) {
-            packageConfig.setModuleName(moduleName);
-        }
-        return packageConfig;
-    }
-
-
-}
+/** * pxys * Copyright (C) 2017-2019 All Rights Reserved. */package com.captain.demo.generator;import com.baomidou.mybatisplus.annotation.DbType;import com.baomidou.mybatisplus.annotation.IdType;import com.baomidou.mybatisplus.generator.AutoGenerator;import com.baomidou.mybatisplus.generator.config.*;import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;import com.baomidou.mybatisplus.generator.config.rules.DateType;import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;import com.baomidou.mybatisplus.generator.config.rules.IColumnType;import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;import org.apache.commons.lang.StringUtils;import org.apache.logging.log4j.LogManager;import org.apache.logging.log4j.Logger;import org.springframework.stereotype.Component;/** * @ClassName: AutoGenerateCode * @Description: * @Author: Monta * @Date: 2019/11/11 下午4:08 **/@Componentpublic class AutoGenerateCode {    private static Logger logger = LogManager.getLogger(AutoGenerateCode.class);    public static void main(String[] args) {        /**         * 根路径，我这里先直接放到此项目中。可以根据自己需要修改制定         */        String basePath = System.getProperty("user.dir");        String parentPackageName = "com.captain.demo";        String moduleName = "mvc";        String[] tableNames = new String[]{"d_user"};        String[] tablePres = new String[]{"d_"};        String jdbcHost = "127.0.0.1";        String jdbcPort = "3306";        String jdbcDatabase = "demo";        String jdbcUrl = String.format("jdbc:mysql://%s:%s/%s?characterEncoding=utf8&useSSL=false&autoReconnect=true&serverTimezone=Asia/Shanghai",                jdbcHost, jdbcPort, jdbcDatabase);        System.out.println(jdbcUrl);        AutoDataSourceConfig autoDataSourceConfig = AutoDataSourceConfig.builder()                .jdbcDriverClassName("com.mysql.jdbc.Driver")                .jdbcPassword("root@localHost")                .jdbcUserName("139566")                .jdbcUrl(jdbcUrl)                .build();        String outputDir = String.format("%s/src/main/java", basePath);        new AutoGenerateCode().generateCode(outputDir, parentPackageName,                moduleName, autoDataSourceConfig, tablePres, tableNames);    }    /**     * 生成代码     *     * @param outputDir         输出目录     * @param parentPackageName 父级包     * @param moduleName        模块名称     * @param dataSourceConfig  数据源配置     * @param tablePres         表前缀     * @param tableNames        待生成表名     */    private void generateCode(String outputDir, String parentPackageName,                              String moduleName, AutoDataSourceConfig dataSourceConfig, String[] tablePres, String[] tableNames) {        try {            AutoGenerator autoGenerator = new AutoGenerator();            autoGenerator.setGlobalConfig(initGlobConfig(outputDir))                    .setDataSource(initDataSource(dataSourceConfig))                    .setStrategy(initStrategyConfig(tablePres, tableNames))                    .setPackageInfo(initPackage(parentPackageName, moduleName))                    .execute();            logger.error(">>>>>>> Generate Code Completed... >>>>>>>");        } catch (Exception e) {            logger.error("Generate Code failed:", e);            throw new RuntimeException(String.format("Generate Code failed"));        }    }    /**     * 全局设置配置     *     * @param outputDir 生成文件输出根目录     * @return GlobalConfig     */    private GlobalConfig initGlobConfig(String outputDir) {        return new GlobalConfig()                .setOutputDir(outputDir)                .setFileOverride(false)                .setOpen(false)                .setEnableCache(false)                .setKotlin(false)                .setSwagger2(true)                .setDateType(DateType.ONLY_DATE)                .setActiveRecord(false)                .setBaseResultMap(true)                .setBaseColumnList(true)                .setMapperName("%sMapper")                .setXmlName("%sMapper")                .setServiceName("I%sService")                .setServiceImplName("%sServiceImpl")                .setControllerName("%sController")                .setAuthor("captain")                .setIdType(IdType.ID_WORKER);    }    /**     * 设置数据源信息     *     * @param dataSourceConfig 数据源配置     * @return DataSourceConfig     */    private DataSourceConfig initDataSource(AutoDataSourceConfig dataSourceConfig) {        return new DataSourceConfig()                .setDbType(DbType.MYSQL)                .setTypeConvert(new MySqlTypeConvert() {                    /**                     * 自定义部分数据库类型对应转换的Java类型，根据个人需要设定                     */                    @Override                    public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {                        if (fieldType.toLowerCase().contains("tinyint")) {                            return DbColumnType.BOOLEAN;                        }                        if (fieldType.toLowerCase().contains("timestamp")) {                            return DbColumnType.DATE;                        }                        return super.processTypeConvert(globalConfig, fieldType);                    }                })                .setDriverName(dataSourceConfig.getJdbcDriverClassName())                .setUrl(dataSourceConfig.getJdbcUrl())                .setUsername(dataSourceConfig.getJdbcUserName())                .setPassword(dataSourceConfig.getJdbcPassword());    }    /**     * 设置生成策略     *     * @param tablePre   表前缀     * @param tableNames 待生成表列表     * @return StrategyConfig     */    private StrategyConfig initStrategyConfig(String[] tablePre, String[] tableNames) {        return new StrategyConfig()                .setNaming(NamingStrategy.underline_to_camel)                .setColumnNaming(NamingStrategy.underline_to_camel)                .setTablePrefix(tablePre)                .setControllerMappingHyphenStyle(true)                .setEntityTableFieldAnnotationEnable(true)                .setEntityLombokModel(true)                .setInclude(tableNames)                .setRestControllerStyle(false);    }    /**     * 设置包配置信息     *     * @param parentPackageName 父级包     * @param moduleName        模块名称     * @return PackageConfig     */    private PackageConfig initPackage(String parentPackageName, String moduleName) {        logger.info(moduleName);        PackageConfig packageConfig = new PackageConfig()                .setParent(parentPackageName)                .setEntity("entity")                .setService("service")                .setServiceImpl("service.impl")                .setMapper("mapper")                .setXml("mapper.xml")                .setController("controller");        if (StringUtils.isNotEmpty(moduleName)) {            packageConfig.setModuleName(moduleName);        }        return packageConfig;    }}
--- src/main/java/com/captain/demo/mvc/controller/UserController.java
+++ src/main/java/com/captain/demo/mvc/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.captain.demo.mvc.controller;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.captain.demo.mvc.entity.User;
-import com.captain.demo.mvc.mapper.UserMapper;
 import com.captain.demo.mvc.service.IUserService;
 import com.captain.demo.utils.Result;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -31,8 +28,8 @@ public class UserController {
 
     @GetMapping("/list")
     @ResponseBody
-    public Result list(){
-        Result result = new Result<>();
+    public Result<List<User>> list() {
+        Result<List<User>> result = new Result<>();
         result.setData(userService.list());
         return result;
     }
